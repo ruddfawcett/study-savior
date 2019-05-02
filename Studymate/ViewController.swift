bfee6b9c37c7255bf2ed420cc1555c3a7d126969
Rudd Fawcett <rudd.fawcett@gmail.com>
2019-05-02 12:49:57 -0400

    Initial Commit

---
+++ Studymate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Studymate
+//
+//  Created by Rudd Fawcett on 5/2/19.
+//  Copyright © 2019 Ally Klionsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
